ddcf5c98045acef134555b99bae51d6be16840fd
kike <utm19020134@utma.edu.mx>
2024-05-10 23:21:29 -0600

    Cambios responsivos4

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function ($) {
   $('.back-to-top').click(function () {
     $('html, body').animate({
       scrollTop: 0
-    }, 500, 'easeInOutExpo');
+    }, 1500, 'easeInOutExpo');
     return false;
   });
 
@@ -98,7 +98,7 @@ jQuery(document).ready(function ($) {
 
         $('html, body').animate({
           scrollTop: target.offset().top - top_space
-        }, 1500, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
 
         if ($(this).parents('.nav-menu').length) {
           $('.nav-menu .menu-active').removeClass('menu-active');
