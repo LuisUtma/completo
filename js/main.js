da41eb04fb78d1baf5c718c76918e689296a337e
LuisPuntoSingular <156908398+LuisPuntoSingular@users.noreply.github.com>
2024-05-16 19:22:29 -0600

    Cambios responsivos finales10

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 jQuery(document).ready(function ($) {
 
   // Back to top button
@@ -38,83 +36,51 @@ jQuery(document).ready(function ($) {
   // Initiate superfish on nav menu
   $('.nav-menu').superfish({
     animation: {
-      opacity: 'show'
+      opacity: 'swing'
     },
     speed: 400
   });
-  
-  function createMobileNav() {
-    // Verifica si existe el contenedor del menú
-    if ($('#nav-menu-container').length) {
-        // Clona el menú y asigna un nuevo ID
-        var $mobile_nav = $('#nav-menu-container').clone().prop({
-            id: 'mobile-nav'
-        });
 
-        // Elimina la clase y asigna un nuevo ID al primer nivel de la lista
-        $mobile_nav.find('> ul').attr({
-            'class': '',
-            'id': 'mobile-nav-ul'
-        });
+  // Mobile Navigation
+  if ($('#nav-menu-container').length) {
+    var $mobile_nav = $('#nav-menu-container').clone().prop({
+      id: 'mobile-nav'
+    });
+    $mobile_nav.find('> ul').attr({
+      'class': '',
+      'id': ''
+    });
+    $('body').append($mobile_nav);
+    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
+    $('body').append('<div id="mobile-body-overly"></div>');
+    $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
 
+    $(document).on('click', '.menu-has-children i', function (e) {
+      $(this).next().toggleClass('menu-item-active');
+      $(this).nextAll('ul').eq(0).slideToggle();
+      $(this).toggleClass("fa-chevron-up fa-chevron-down");
+    });
 
-        // Agrega el menú móvil al encabezado (o cualquier otro contenedor que prefieras)
-        $('header').append($mobile_nav);
+    $(document).on('click', '#mobile-nav-toggle', function (e) {
+      $('body').toggleClass('mobile-nav-active');
+      $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+      $('#mobile-body-overly').toggle();
+    });
 
-        // Agrega el botón de toggle al principio del body
-        $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
+    $(document).click(function (e) {
+      var container = $("#mobile-nav, #mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+      }
+    });
+  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
+    $("#mobile-nav, #mobile-nav-toggle").hide();
+  }
 
-        // Agrega un overlay al final del body
-        $('body').append('<div id="mobile-body-overly"></div>');
-
-        // Agrega flechas de despliegue a los elementos con submenús
-        $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
-
-        // Maneja los eventos de clic en los íconos de flecha para mostrar/ocultar submenús
-        $(document).on('click', '.menu-has-children i', function(e) {
-            $(this).next().toggleClass('menu-item-active');
-            $(this).nextAll('ul').eq(0).slideToggle();
-            $(this).toggleClass("fa-chevron-up fa-chevron-down");
-        });
-
-        // Maneja el evento de clic en el botón de toggle para mostrar/ocultar el menú móvil
-        $(document).on('click', '#mobile-nav-toggle', function(e) {
-            $('body').toggleClass('mobile-nav-active');
-            $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-            $('#mobile-body-overly').toggle();
-        });
-
-        // Maneja el evento de clic en el enlace "Change Language" dentro del menú móvil
-      
-
-        // Maneja el evento de clic fuera del menú para cerrarlo
-        $(document).click(function(e) {
-            var container = $("#mobile-nav, #mobile-nav-toggle");
-            if (!container.is(e.target) && container.has(e.target).length === 0) {
-                if ($('body').hasClass('mobile-nav-active')) {
-                    $('body').removeClass('mobile-nav-active');
-                    $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-                    $('#mobile-body-overly').fadeOut();
-                }
-            }
-        });
-    } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
-        // Oculta el menú móvil si no existe el contenedor del menú
-        $("#mobile-nav, #mobile-nav-toggle").hide();
-    }
-}
-
-
-
-
-
-
-  // Llama a la función cuando el documento esté completamente cargado
-  $(document).ready(function() {
-    createMobileNav();
-  });
-  
-  
   // Smooth scroll for the menu and links with .scrollto classes
   $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -132,7 +98,7 @@ jQuery(document).ready(function ($) {
 
         $('html, body').animate({
           scrollTop: target.offset().top - top_space
-        }, 500, 'easeInOutExpo');
+        }, 10, 'easeInOutExpo');
 
         if ($(this).parents('.nav-menu').length) {
           $('.nav-menu .menu-active').removeClass('menu-active');
@@ -206,27 +172,3 @@ jQuery(document).ready(function ($) {
 
 
 });
-
-
-function printMenuClasses() {
-  // Espera a que el documento esté listo
-  $(document).ready(function() {
-    // Espera a que el menú móvil esté listo
-    $(document).on('click', '#mobile-nav-toggle', function() {
-      // Itera sobre todos los elementos del menú y muestra sus clases
-      $('#mobile-nav').find('*').each(function() {
-        var classes = $(this).attr('class');
-        if (classes) {
-          console.log("Clases del elemento:", classes);
-        }
-      });
-    });
-  });
-}
-
-// Llama a la función cuando el documento esté listo
-printMenuClasses();
-
-
-
-
